63aef8c672390504f021c80117fdab20607c2915
Alexandre DELTOUR <alexandre.deltour@geodis.com>
2022-02-17 16:07:41 +0100

    buckets3

--- bucketfront.tf
+++ bucketfront.tf
@@ -1,30 +1,15 @@
 resource "aws_s3_bucket" "front" {
   bucket = "front-eql-xchange"
+ acl = "public-read"
+  
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
 
+    force_destroy = true
+  }
   
 }
 
-resource "aws_s3_bucket_policy" "front" {
-  bucket = aws_s3_bucket.front.id
 
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject",
-                 "s3:GetObjectVersion"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.front.id}/*"
-            ]
-        }
-    ]
-}
-POLICY
-}
 
